070fe83d1e015bd850c1ed67aee93a5a14483c36
duynvph15542 <duynvph15542@fpt.edu.vn>
2022-03-08 13:35:06 +0700

    update

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+export{}
+type Product={
+    id:number;
+    name:string;
+}
+const a:number = 20;
+const b:number = 30;
+const name:string = "Nguyễn Văn Duy";
+const age:number =20;
+const status:boolean=true;
+const info:Product={id:1,name:'a'};
+const stringArr:string[]=["a","b","c"];
+const numberArr:number[]=[1,2,3,4,5];
+const objectAr:Product[]=[{id:1},{id:2}];
+function sum(numA:number,numB:number):number{
+    return numA + numB;
+}
+sum(a,b);
+console.log(age);
